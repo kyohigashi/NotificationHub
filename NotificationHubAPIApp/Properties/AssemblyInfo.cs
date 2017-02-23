b3d5d5698767b040e9d0cf9f7999fd8abeb0daef
matt.choi <matt.choi@pacim.local>
2017-02-23 12:35:25 +0800

    Config update.

--- NotificationHubAPIApp/Properties/AssemblyInfo.cs
+++ NotificationHubAPIApp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NotificationHubAPIAppCLP")]
+[assembly: AssemblyTitle("NotificationHubAPIApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NotificationHubAPIAppCLP")]
+[assembly: AssemblyProduct("NotificationHubAPIApp")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
